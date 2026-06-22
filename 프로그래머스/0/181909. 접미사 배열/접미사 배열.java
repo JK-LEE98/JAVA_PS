22ef3978404ebaf7e7e672beab127e1bb0bc8ff0
JK-LEE98 <abc9541@gmail.com>
2026-06-22 19:38:01 +0900

    [level 0] Title: 접미사 배열, Time: 0.51 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] arr = new String[my_string.length()];
+        
+        for (int i=0; i<my_string.length(); i++) {
+            arr[i] = my_string.substring(i,arr.length);    
+        }
+        
+        Arrays.sort(arr);
+        
+        return arr;
+    }
+}
